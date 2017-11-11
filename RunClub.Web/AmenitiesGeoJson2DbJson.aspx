ad5e2d2bfc23c3d35ec1d07869d26fa43a42e657
Gareth Evans <agrath@gmail.com>
2017-11-11 16:17:41 +1300

    Quick and dirty json converter for OSM overpass json to our places of interest format

---
+++ RunClub.Web/AmenitiesGeoJson2DbJson.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="AmenitiesGeoJson2DbJson.aspx.cs" Inherits="RunClub.Web.AmenitiesGeoJson2DbJson" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox runat="server" ID="uxGeoJson" TextMode="MultiLine" Placeholder="Paste Geo-Json here" Rows="20" Columns="200"></asp:TextBox><br /><br />
+            <asp:Button runat="server" id="uxConvertButton" Text="vvvv Convert to db.json vvvv" OnClick="uxConvertButton_Click" /><br /><br />
+            <asp:TextBox runat="server" ID="uxDbJson" TextMode="MultiLine" Placeholder="Output will appear here" Enabled="false" Rows="20" Columns="200"></asp:TextBox><br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ RunClub.Web/AmenitiesGeoJson2DbJson.aspx.cs
@@ -0,0 +1,107 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RunClub.Web
+{
+    public partial class AmenitiesGeoJson2DbJson : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void uxConvertButton_Click(object sender, EventArgs e)
+        {
+            Dictionary<string, string> typeMap = new Dictionary<string, string>();
+            typeMap.Add("toilets", "public-bathroom");
+            typeMap.Add("drinking_water", "water-fountain");
+            typeMap.Add("viewpoint", "photo-camera");
+            List<DbJsonPlace> places = new List<DbJsonPlace>();
+            if (!string.IsNullOrWhiteSpace(uxGeoJson.Text))
+            {
+                try
+                {
+                    var geoJson = JsonConvert.DeserializeObject<Rootobject>(uxGeoJson.Text);
+                    if (geoJson != null)
+                    {
+                        foreach (var feature in geoJson.features)
+                        {
+                            float latitude = feature.geometry.coordinates[1];
+                            float longitude = feature.geometry.coordinates[0];
+                            string geoType = feature.properties.amenity ?? feature.properties.tourism;
+                            string type = null;
+                            if (typeMap.TryGetValue(geoType, out type))
+                            {
+                                places.Add(new DbJsonPlace() { Latitude = latitude, Longitude = longitude, Type = type });
+                            }
+                        }
+
+                        uxDbJson.Text = JsonConvert.SerializeObject(places.ToArray(), Formatting.Indented);
+
+                        return;
+                    }
+                    else
+                    {
+                        uxDbJson.Text = "Could not deserialize geo json data";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    uxDbJson.Text = ex.ToString();
+                }
+            }
+            else
+            {
+                uxDbJson.Text = "Missing geo json data";
+            }
+        }
+    }
+    public class DbJsonPlace
+    {
+        [JsonProperty("type")]
+        public string Type { get; set; }
+        [JsonProperty("latitude")]
+        public float Latitude { get; set; }
+        [JsonProperty("longitude")]
+        public float Longitude { get; set; }
+    }
+
+    public class Rootobject
+    {
+        public string type { get; set; }
+        public string generator { get; set; }
+        public string copyright { get; set; }
+        public DateTime timestamp { get; set; }
+        public Feature[] features { get; set; }
+    }
+
+    public class Feature
+    {
+        public string type { get; set; }
+        public Properties properties { get; set; }
+        public Geometry geometry { get; set; }
+        public string id { get; set; }
+    }
+
+    public class Properties
+    {
+        public string id { get; set; }
+        public string amenity { get; set; }
+        public string tourism { get; set; }
+        public string name { get; set; }
+        public string nameen { get; set; }
+        public string namezh { get; set; }
+    }
+
+    public class Geometry
+    {
+        public string type { get; set; }
+        public float[] coordinates { get; set; }
+    }
+
+}
