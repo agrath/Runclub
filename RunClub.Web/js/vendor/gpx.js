112097c560d7e55e753fa03de18b9b6f84bb4ecc
Gareth Evans <agrath@gmail.com>
2017-12-29 12:16:28 +1300

    fix elevation data read in rpt format

--- RunClub.Web/js/vendor/gpx.js
+++ RunClub.Web/js/vendor/gpx.js
@@ -212,7 +212,7 @@ GPXParser.prototype.extractElevationData = function () {
         var route = routes[i];
         var result = [];
 
-        var routepoints = segment.getElementsByTagName("rtept");
+        var routepoints = route.getElementsByTagName("rtept");
         if (routepoints.length == 0) {
             break;
         }
