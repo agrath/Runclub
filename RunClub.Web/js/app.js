ac9ac946c85285ae9a14702c9ae9dece0e840389
Gareth Evans <agrath@gmail.com>
2017-12-29 14:59:47 +1300

    Update calendar, fix issue when no meetup link available

--- RunClub.Web/js/app.js
+++ RunClub.Web/js/app.js
@@ -1,4 +1,10 @@
-﻿var app = angular.module('routesApp', ['ngSanitize', 'ngRoute', 'dibari.angular-ellipsis']);
+﻿/*
+todo:
+- change sign up to reflect joining club, not sunday run
+- add sign up form to sunday run (just name & email)
+*/
+
+var app = angular.module('routesApp', ['ngSanitize', 'ngRoute', 'dibari.angular-ellipsis']);
 app.run(function ($rootScope) {
     $rootScope.$on('$locationChangeSuccess', function (event, to, from) {
         var url = to.toString();
